829fd1d061f5f9af28cb044c3d365b6de987fbbd
Maciej Kujawski <2988861+maciejkujawski@users.noreply.github.com>
2018-02-19 17:32:58 +0100

    documentation of sample-service

--- sample-service/src/main/java/org/sample/service/endpoint/SampleService.java
+++ sample-service/src/main/java/org/sample/service/endpoint/SampleService.java
@@ -1,21 +1,38 @@
 package org.sample.service.endpoint;
 
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.apache.cxf.ext.logging.Logging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Service;
+
 /**
  * @author Maciej Kujawski <kujawski.maciej@gmail.com>
  */
+@Service
+@RefreshScope
+@Path("/sample")
+@Logging
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-@Path("/sample")
-public interface SampleService
+public class SampleService
 {
+	@Value("${common.property:default}")
+	private String commonProperty;
+
 	@GET
 	@Path("/hello")
-	Hello sayHello();
+	public Hello sayHello()
+	{
+		Hello hello = new Hello();
+		hello.setMessage("Spring Cloud with CXF, Hello World!");
 
+		return hello;
+	}
 }
